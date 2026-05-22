39796798d941cb0329b50b1f8935ae591b8b1579
Fernando Muñoz <fmunoz@exisoft.com.ar>
2026-05-22 08:58:37 -0300

    se agregan killzones y limites del nivel

--- final_boos/scape-from-here/scripts/level.gd
+++ final_boos/scape-from-here/scripts/level.gd
@@ -5,3 +5,9 @@ var keys: int
 func _on_character_body_2d_grab_keys(cantidad: int) -> void:
 	keys += 1
 	print(cantidad)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		GameManager.handle_pause()
+		get_tree().reload_current_scene()
