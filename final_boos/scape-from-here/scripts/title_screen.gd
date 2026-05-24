0c44378d4056ca16e35328fe7a8bfa0117d19eeb
Fernando Muñoz <fmunoz@exisoft.com.ar>
2026-05-23 23:44:36 -0300

    se agregan particular en el menu del titulo y se ajusta el manejo de focus en los botones

--- final_boos/scape-from-here/scripts/title_screen.gd
+++ final_boos/scape-from-here/scripts/title_screen.gd
@@ -2,15 +2,12 @@ extends Control
 
 @onready var navigation_sound: AudioStreamPlayer = $sfx/navigation_sound
 @onready var start_game: Button = $"VBoxContainer/start game"
+@onready var credits: Button = $VBoxContainer/credits
+@onready var quit_game: Button = $"VBoxContainer/quit game"
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	start_game.grab_focus()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
 
 func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level.tscn")	
@@ -22,14 +19,25 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_start_game_mouse_entered() -> void:
+	start_game.grab_focus()
 	navigation_sound.play()
 
 func _on_credits_mouse_entered() -> void:
+	credits.grab_focus()
 	navigation_sound.play()
 
 func _on_quit_game_mouse_entered() -> void:
+	quit_game.grab_focus()
 	navigation_sound.play()
 
-
 func _on_start_game_focus_entered() -> void:
-	pass
+	start_game.grab_focus()
+	navigation_sound.play()
+
+func _on_credits_focus_entered() -> void:
+	credits.grab_focus()
+	navigation_sound.play()
+
+func _on_quit_game_focus_entered() -> void:
+	quit_game.grab_focus()
+	navigation_sound.play()
