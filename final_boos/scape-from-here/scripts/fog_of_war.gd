dbde5fd6f40ceeb445a4d2842afe3677e1235062
Fernando Muñoz <fmunoz@exisoft.com.ar>
2026-06-21 16:35:53 -0300

    implementacion de niebla de guerra

---
+++ final_boos/scape-from-here/scripts/fog_of_war.gd
@@ -0,0 +1,14 @@
+extends Area2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		sprite_2d.set_visible(false)
+	print("algo")	
+	
