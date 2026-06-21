b2327e25ff73b22b7cd3204ee9c23b543388b7d3
Fernando Muñoz <fmunoz@exisoft.com.ar>
2026-06-21 13:38:31 -0300

    se agrega daño por contacto y plataformas a zonas sin salida

--- final_boos/scape-from-here/scripts/character_body_2d.gd
+++ final_boos/scape-from-here/scripts/character_body_2d.gd
@@ -12,7 +12,6 @@ enum STATE {IDLE, RUN, JUMP, FALL, ATTACK, DEATH, DOUBLE_JUMP, HIT}
 var current_state : STATE
 var live: int = 100
 
-#@onready var run_sfx: AudioStreamPlayer2D = $sfx/run_sfx
 @onready var attack_sfx: AudioStreamPlayer2D = $sfx/attack_sfx
 @onready var animated_sprite_2d : AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox_2d: CollisionShape2D = $hitbox/CollisionShape2D
@@ -28,12 +27,12 @@ var max_jumps: int = 2
 var jumps_left: int = 0
 
 func _ready() -> void:
-	GameManager.minimap_show.connect(show_on_minimap)
 	current_state = STATE.IDLE
 	_enter_state()
 
 func _physics_process(delta: float) -> void:
 	_update_state(delta)
+
 
 func _set_state(new_state: STATE) -> void:
 	if current_state == new_state:
@@ -41,7 +40,8 @@ func _set_state(new_state: STATE) -> void:
 	_exit_state()
 	current_state = new_state
 	_enter_state()
-	
+
+
 func _enter_state() -> void:
 	match current_state:
 		STATE.IDLE:
@@ -80,7 +80,8 @@ func _enter_state() -> void:
 			animated_sprite_2d.play("death")
 			await animated_sprite_2d.animation_finished
 			queue_free()
-			
+
+
 func _exit_state() -> void:
 	match current_state:
 		STATE.ATTACK:
@@ -92,7 +93,8 @@ func _exit_state() -> void:
 				animated_sprite_2d.animation_finished.disconnect(_on_hit_animation_finished)
 		STATE.RUN:
 			pass
-			
+
+
 func _update_state(delta: float) -> void:
 	var direction = Input.get_axis("ui_left", "ui_right") # 1 right -1 left
 	if direction != 0 and current_state != STATE.ATTACK:
@@ -168,6 +170,7 @@ func _update_state(delta: float) -> void:
 			_aplicar_gravedad(delta)
 			move_and_slide()
 
+
 func _aplicar_gravedad(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
@@ -197,16 +200,17 @@ func receive_damage(enemy_damage: int,enemy_pos: Vector2) -> void:
 		velocity.y = -120.0  
 		_set_state(STATE.HIT)
 
+
 func _on_smoke_vfx_finished() -> void:
 	double_jump_vfx.visible = false
 	if double_jump_vfx.animation_finished.is_connected(_on_smoke_vfx_finished):
 		double_jump_vfx.animation_finished.disconnect(_on_smoke_vfx_finished)
 
 
-func show_on_minimap() -> void:
-	pass
-	#icon.visible = !icon.visible
-
 func _on_hit_animation_finished() -> void:
 	if current_state == STATE.HIT:
 		_set_state(STATE.IDLE)
+
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+	self.receive_damage(5,body.global_position)
